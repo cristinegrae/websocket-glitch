95e6a68c11889441b45a07ad4afc2da032dc869e
cristinegrae <160692737+cristinegrae@users.noreply.github.com>
2024-02-21 11:25:20 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,45 @@
-import { WebSocketServer } from "ws";
+import { WebSocketServer } from 'ws';
+import url from 'url';
 
 var wss = new WebSocketServer({ port: 8100 });
 
-wss.on("connection", function connection(ws) {
-  ws.on("message", function (message) {
+wss.getUniqueID = function () {
+  function s4() {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  }
+
+  return s4() + s4() + '-' + s4();
+};
+
+wss.on('connection', function connection(ws, req) {
+  const parameters = url.parse(req.url, true);
+
+  if (!parameters.query.email) {
+    ws.close();
+  }
+
+  ws.id = wss.getUniqueID();
+  ws.email = parameters.query.email;
+
+  wss.clients.forEach(function each(client) {
+    if (ws.email !== client.email) {
+      ws.send('Client ' + client.email + ' with ID ' + ws.id + ' connected');
+    }
+  });
+
+  wss.broadcast('Client ' + ws.email + ' with ID ' + ws.id + ' connected');
+
+  ws.on('message', function (message) {
     wss.broadcast(message);
+  });
+
+  ws.on('close', function close() {
+    wss.clients.forEach(function each(client) {
+      ws.send('Client ' + client.email + ' with ID ' + ws.id + ' disconnected');
+    });
+    console.log('disconnected');
   });
 });
 
@@ -15,6 +50,6 @@ wss.broadcast = function broadcast(msg) {
   });
 };
 
-wss.on('listening',()=>{
-    console.log('listening on 8100')
- })
+wss.on('listening', () => {
+  console.log('listening on 8100');
+});
