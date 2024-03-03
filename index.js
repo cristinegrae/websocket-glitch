74c5cc2d5006a3c6df94858ea6d85a262b769d3e
crstngrace <106168050+crstngrace@users.noreply.github.com>
2024-03-03 20:20:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-import { WebSocketServer } from 'ws';
-import url from 'url';
+import { WebSocketServer } from "ws";
+import url from "url";
 
 var wss = new WebSocketServer({ port: 8100 });
 
@@ -10,10 +10,10 @@ wss.getUniqueID = function () {
       .substring(1);
   }
 
-  return s4() + s4() + '-' + s4();
+  return s4() + s4() + "-" + s4();
 };
 
-wss.on('connection', function connection(ws, req) {
+wss.on("connection", function connection(ws, req) {
   const parameters = url.parse(req.url, true);
 
   if (!parameters.query.email) {
@@ -25,21 +25,50 @@ wss.on('connection', function connection(ws, req) {
 
   wss.clients.forEach(function each(client) {
     if (ws.email !== client.email) {
-      ws.send('Client ' + client.email + ' with ID ' + ws.id + ' connected');
+      // ws.send("Client " + client.email + " with ID " + ws.id + " connected");
+      ws.send(
+        JSON.stringify({
+          type: "connect",
+          email: client.email,
+          id: ws.id,
+        })
+      );
     }
   });
 
-  wss.broadcast('Client ' + ws.email + ' with ID ' + ws.id + ' connected');
+  // wss.broadcast("Client " + ws.email + " with ID " + ws.id + " connected");
+  wss.broadcast(
+    JSON.stringify({
+      type: "connect",
+      email: ws.email,
+      id: ws.id,
+    })
+  );
 
-  ws.on('message', function (message) {
-    wss.broadcast(message);
+  ws.on("message", function (message) {
+    // wss.broadcast(message);
+    wss.broadcast(
+      JSON.stringify({
+        type: "message",
+        email: ws.email,
+        id: ws.id,
+        message: message.toString(),
+      })
+    );
   });
 
-  ws.on('close', function close() {
+  ws.on("close", function close() {
     wss.clients.forEach(function each(client) {
-      ws.send('Client ' + client.email + ' with ID ' + ws.id + ' disconnected');
+      // ws.send("Client " + client.email + " with ID " + ws.id + " disconnected");
+      ws.send(
+        JSON.stringify({
+          type: "disconnect",
+          email: client.email,
+          id: ws.id,
+        })
+      );
     });
-    console.log('disconnected');
+    console.log("disconnected");
   });
 });
 
@@ -50,6 +79,6 @@ wss.broadcast = function broadcast(msg) {
   });
 };
 
-wss.on('listening', () => {
-  console.log('listening on 8100');
+wss.on("listening", () => {
+  console.log("listening on 8100");
 });
